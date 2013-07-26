03273f2dad656b29c3d02b8bff887a956946926d
Amit Pundir <amit.pundir@linaro.org>
2013-07-27 01:42:30 +0530

    libstagefright: audioflinger: use no strict aliasing for now
    Change-Id: Ie16c84cd63418800953ee8105b0dc338d87e8aa5
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: gamerman123x <19superguy98@gmail.com>

--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -59,6 +59,11 @@ LOCAL_SRC_FILES:=               \
 
 LOCAL_SRC_FILES += StateQueue.cpp
 
+# uncomment for debugging timing problems related to StateQueue::push()
+LOCAL_CFLAGS += -DSTATE_QUEUE_DUMP
+
+LOCAL_CFLAGS += -fno-strict-aliasing
+
 LOCAL_C_INCLUDES := \
     $(TOPDIR)frameworks/av/services/audiopolicy \
     $(call include-path-for, audio-effects) \
