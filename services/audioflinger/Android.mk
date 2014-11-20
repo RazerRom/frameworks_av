0dac3bb5eeb3367a636bfb3e0f8fde55d0f71797
Dan Albert <danalbert@google.com>
2014-11-20 11:31:17 -0800

    C++11 compatibility.
     * Fix string literal concatenation to not be interpreted as UD
       literals.
     * Add constexpr compatibility for non-integral static members.
     * Use __typeof__ instead of typeof (should become decltype once this
       actually becomes C++11).
     * Add an appropriate cast for atomic_uintptr_t, since moving to C++11
       means moving from <stdatomic.h> to <atomic>, which has better
       typechecking (hooray for not macros!).
    
    Bug: 18466763
    Change-Id: I9561dcb2526578687819ff85421ba80d8e1a9694
    
    Also fix some compilation errors by enforcing C++11.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -93,7 +93,7 @@ endif
 endif
 #QTI Resampler
 
-LOCAL_CFLAGS += -Os
+LOCAL_CFLAGS += -Os -std=gnu++11
 
 LOCAL_MODULE:= libaudioflinger
 LOCAL_32_BIT_ONLY := true
@@ -197,6 +197,7 @@ endif
 #QTI Resampler
 
 LOCAL_MODULE := libaudioresampler
+LOCAL_CFLAGS += -std=gnu++11
 
 include $(BUILD_SHARED_LIBRARY)
 
